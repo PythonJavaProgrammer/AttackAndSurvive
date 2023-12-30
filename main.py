3b7481a895ab7daf3a3685bd6cb9ce5bda877e9b
Jethro Karusseit <jethro.karusseit@onlineschool.co.za>
2023-12-30 11:39:53 +0200

    Added function prompt and description

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from ask_methods import *
 
+
+def prompt(text: str):
+    """
+    This function prompts the user at the required amount of text
+    to slow the game down and give the user time to read
+    :param text:  (str) The text for the prompt
+    :return: None
+    """
+    trash_variable = input(text)
+    del trash_variable
+
+
 if __name__ == "__main__":
-    ans = ask_numeric("Landon", "Tom", "Seth", "Other", prompt="Is your name: ")
-    print(ans)
+    pass
