31a1ec04e1749f12fe76040a6573736bbad09e72
PythonJavaProgrammer <118815056+PythonJavaProgrammer@users.noreply.github.com>
2024-01-03 07:49:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def prompt(text: str):
     """
     This function prompts the user at the required amount of text
     to slow the game down and give the user time to read
-    :param text:  (str) The text for the prompt
+    :param text: (str) The text for the prompt
     :return: None
     """
     trash_variable = input(text)
