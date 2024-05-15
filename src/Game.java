3ba64f93c1acc20c11b4395cab560da2ee65a703
Jethro Karusseit <jethro.karusseit@onlineschool.co.za>
2024-05-15 17:30:19 +0200

    Completed Project Setup: added all drivers and finished filling README.md with all details

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Game {
+    Game() {
+        //create a game
+    }
+    static void save() {
+        //save game to database
+    }
+
+    static void load(String name) {
+        //load game from database
+    }
+}
